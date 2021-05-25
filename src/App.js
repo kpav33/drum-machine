26cb068e771cfaee55770d0172c18c6f20ac8fc7
Klemen Pavlovič <klemenpavlovic@gmail.com>
2021-05-25 16:17:08 +0200

    Initial app layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="panel" id="drum-machine">
+        <h1>Drum Machine</h1>
+        <div className="buttonsPad">
+          <button>1</button>
+          <button>1</button>
+          <button>1</button>
+          <button>1</button>
+          <button>1</button>
+          <button>1</button>
+          <button>1</button>
+          <button>1</button>
+          <button>1</button>
+        </div>
+        <p id="display">Display pad</p>
+        <p>Volume bar</p>
+        <p>Bottom text</p>
+      </div>
     </div>
   );
 }
